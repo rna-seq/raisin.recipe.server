dbc957a6794adfc917e0cec9eb37a43671b95260
Maik R��der <maikroeder@gmail.com>
2012-02-19 21:14:26 +0000

    =create paths

--- raisin/recipe/server/__init__.py
+++ raisin/recipe/server/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """Recipe raisin.recipe.server"""
 
+import os
 from raisin.recipe.server import server
 
 
@@ -14,6 +15,15 @@ class Recipe(object):
     def install(self):
         staging = self.buildout['transform']['staging']
         buildout_directory = self.buildout['buildout']['directory']
+
+        for etc_path in ['etc/projects', 
+                         'etc/databases',
+                         'etc/pyramid',
+                         'etc/misc']:
+            path = os.path.join(buildout_directory, etc_path)
+            if not os.path.exists(path):
+                os.makedirs(path)
+
         server.main(self.buildout, buildout_directory, staging)
 
     def update(self):
